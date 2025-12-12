16c91d8507ed07021d1cb1417023b164fd145f8e
Matheus Rodrigues <ec.mattrodrigues@gmail.com>
2025-12-12 11:52:11 -0300

    feat: Created User Entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,34 @@
+export class User {
+  id: string;
+  email: string;
+  password: string;
+  name: string;
+  type: string;
+  phones: string[];
+  address: string;
+  createdAt: Date;
+  updatedAt: Date;
+  items: any[];
+  orders: any[];
+
+  constructor(
+    id: string,
+    email: string,
+    password: string,
+    name: string,
+    type: string,
+    phones: string[],
+  ) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+    this.name = name;
+    this.type = type;
+    this.phones = phones;
+    this.address = '';
+    this.createdAt = new Date(); // Set creation date to current date
+    this.updatedAt = new Date(); // Set update date to current date
+    this.items = []; // Initialize items as an empty array
+    this.orders = []; // Initialize orders as an empty array
+  }
+}
