72de923c1bd76d7b7604d213ee1cbc2c4f2628b0
Sara Ferreira <sara.f.araujo12@gmail.com>
2025-12-13 20:38:38 -0300

    feat(items): created controller

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ItemsService } from './item.service';
 import { ItemsRepository } from './item.repository';
+import { ItemsController } from './item.controller';
 
 @Module({
     imports: [PrismaModule],
     providers: [ItemsService, ItemsRepository],
+    controllers: [ItemsController],
 })
 export class ItemModule { }
