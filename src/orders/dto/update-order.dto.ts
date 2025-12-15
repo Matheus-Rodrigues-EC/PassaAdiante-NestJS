9e56bcd637bea3bd1ced972e989b3288f8c087d6
Holivane <holivane@gmail.com>
2025-12-15 11:53:08 -0300

    fix: orders UpdateDto

--- src/orders/dto/update-order.dto.ts
+++ src/orders/dto/update-order.dto.ts
@@ -1,14 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateOrderDto } from './create-order.dto';
-import { IsDate, IsNotEmpty, IsString } from 'class-validator';
-import { OrderStatus } from '../../generated/prisma/enums';
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {
-    @IsNotEmpty()
-    @IsString()
-    status: OrderStatus;
-
-    @IsNotEmpty()
-    @IsDate()
-    updatedAt: Date;
-}
+export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
